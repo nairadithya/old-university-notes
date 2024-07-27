f2461568e28b21f7d577a06aeb60879fe76e2fc0
Adithya Nair <adithyanair121@gmail.com>
2024-07-27 21:09:43 +0530

    Interesting commit

---
+++ Modelling/.auctex-auto/main.el
@@ -0,0 +1,30 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("report" "twoside")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("svg" "")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "preamble"
+    "report"
+    "rep10"
+    "svg")
+   (LaTeX-add-environments
+    '("explanation" LaTeX-env-args ["argument"] 0)
+    '("proof" LaTeX-env-args ["argument"] 0)))
+ :latex)
+
