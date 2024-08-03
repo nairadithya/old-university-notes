d7510c78d663f3549ce65c9b2e1409a5d664a8f6
Adithya Nair <adithyanair121@gmail.com>
2024-08-03 11:54:39 +0530

    3rd August commit

---
+++ college-notes/Compressor.java
@@ -0,0 +1,13 @@
+// Read a character array and store it in a smaller array.
+import java.util.*;
+class Compressor {
+
+    public static void main(String[] args) {
+        System.out.print("success");
+    }
+
+    int[] compress(char[] inputString){
+        int length = inputString.length;
+        int[] a = new int[length];
+    }
+}
---
+++ college-notes/PoliceRecruitment.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class PoliceRecruitment {
+
+    public static void main(String[] args) {
+        System.out.print("success");
+    }
+
+
+// ID, Height, Running, Long Jump, High Jump, Average
+    void PoliceRecruitment(int length){
+        int[] RankedList = new int[length];
+        int[][] RecruitmentList = new int[length][6];
+    }
+    void main(){
+       PoliceRecruitment PR = new PoliceRecruitment(1);
+       PR.RecruitmentList[0][1] = 1239021;
+    }
+    void Sort(int[] RL);
+    void ScoreCalculator(int[] RL){
+        for(int i = 0;i<RL.length;i++){
+            RL[i][5] = RL[i][1] + RL[i][2]+RL[i][3]+RL[i][4];
+        }
+}
+}
