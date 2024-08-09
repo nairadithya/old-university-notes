a61eea08059a01701df49de91fe850032902ddc3
Adithya Nair <adithyanair121@gmail.com>
2024-08-09 09:26:57 +0530

    Weekly Update 2

--- College Notes/.auctex-auto/preamble.el
+++ College Notes/.auctex-auto/preamble.el
@@ -4,7 +4,7 @@
  "preamble"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("xcolor" "dvipsnames" "pdftex") ("import" "") ("geometry" "tmargin=2cm" "rmargin=0.5in" "lmargin=0.5in" "margin=0.85in" "bmargin=2cm" "footskip=.2in") ("cantarell" "") ("hyperref" "") ("graphicx" "") ("float" "") ("multicol" "") ("amsmath" "") ("amsfonts" "") ("mathtools" "") ("amsthm" "") ("amssymb" "") ("cancel" "") ("caption" "") ("pdfpages" "") ("algorithm2e" "ruled" "vlined" "linesnumbered") ("tikzsymbols" "") ("fncychap" "Lenny") ("mdframed" "framemethod=TikZ") ("tikz" "") ("tikz-cd" "") ("siunitx" "") ("fancyhdr" "") ("listings" "") ("varwidth" "") ("thmtools" "") ("tcolorbox" "most" "many" "breakable")))
+                     '(("xcolor" "dvipsnames" "pdftex") ("geometry" "tmargin=2cm" "rmargin=1in" "lmargin=1in" "margin=0.85in" "bmargin=2cm" "footskip=.2in") ("cantarell" "") ("hyperref" "") ("graphicx" "") ("float" "") ("multicol" "") ("amsmath" "") ("amsfonts" "") ("mathtools" "") ("amsthm" "") ("amssymb" "") ("cancel" "") ("caption" "") ("pdfpages" "") ("algorithm2e" "ruled" "vlined" "linesnumbered") ("tikzsymbols" "") ("bookmark" "") ("siunitx" "") ("mdframed" "framemethod=TikZ") ("tikz" "") ("tikz-cd" "") ("pgfplots" "") ("titling" "") ("eso-pic" "") ("xifthen" "") ("import" "") ("fancyhdr" "") ("xargs" "") ("todonotes" "colorinlistoftodos") ("varwidth" "") ("thmtools" "") ("tcolorbox" "most" "many" "breakable")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
@@ -16,8 +16,8 @@
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
+    "intro"
     "xcolor"
-    "import"
     "geometry"
     "cantarell"
     "hyperref"
@@ -34,34 +34,81 @@
     "pdfpages"
     "algorithm2e"
     "tikzsymbols"
-    "fncychap"
+    "bookmark"
+    "siunitx"
     "mdframed"
     "tikz"
     "tikz-cd"
-    "siunitx"
+    "pgfplots"
+    "titling"
+    "eso-pic"
+    "xifthen"
+    "import"
     "fancyhdr"
-    "listings"
+    "xargs"
+    "todonotes"
     "varwidth"
     "thmtools"
     "tcolorbox")
    (TeX-add-symbols
+    '("incfig" ["argument"] 1)
+    '("lesson" ["argument"] 2)
     '("createnewcoloredtheoremstyle" 4)
     '("createnewtheoremstyle" 3)
-    '("incfig" 1)
+    '("correct" 2)
+    '("circled" 1)
+    "definegraybackground"
+    "wrapfill"
+    "resetcounters"
+    "forcenewpage"
+    "createintro"
+    "listnotes"
+    "class"
     "implies"
     "impliedby"
     "iff"
-    "epsilon")
+    "epsilon"
+    "svgwidth")
    (LaTeX-add-environments
+    "multicolmulticols"
     '("solution" LaTeX-env-args ["Text"] ["Text"])
     '("question" LaTeX-env-args ["Text"] ["Text"])
     '("definition" LaTeX-env-args ["Text"] ["Text"])
+    '("multicols" LaTeX-env-args "Text" ["Text"])
     '("explanation" LaTeX-env-args ["argument"] 0)
     '("proof" LaTeX-env-args ["argument"] 0))
+   (LaTeX-add-counters
+    "lecturecounter")
+   (LaTeX-add-lengths
+    "thickarrayrulewidth")
    (LaTeX-add-xcolor-definecolors
+    "myg"
+    "myb"
+    "myr"
+    "mytheorembg"
+    "mytheoremfr"
+    "mylenmabg"
+    "mylenmafr"
+    "mypropbg"
+    "mypropfr"
+    "myrefbg"
+    "myreffr"
+    "myexamplebg"
+    "myexamplefr"
+    "myexampleti"
+    "mydefinitbg"
+    "mydefinitfr"
+    "notesgreen"
+    "myp"
+    "mygr"
+    "myred"
+    "myyellow"
+    "myexercisebg"
+    "myexercisefg"
+    "reallylightgray"
+    "correct"
     "myblue"
     "mygreen"
-    "myred"
     "mypurple"
     "definition"
     "theorem"
@@ -73,14 +120,13 @@
     "corollary"
     "proof")
    (LaTeX-add-amsthm-newtheorems
+    "note"
     "notation"
     "previouslyseen"
     "problem"
     "observe"
     "property"
     "intuition")
-   (LaTeX-add-listings-lstdefinestyles
-    "myStyle")
    (LaTeX-add-thmtools-declaretheoremstyles
     "#1")
    (LaTeX-add-thmtools-declaretheorems
@@ -92,7 +138,10 @@
     "lemma"
     "exercise"
     "replacementproof"
-    "expl")
+    "expl"
+    "definition"
+    "question"
+    "solution")
    (LaTeX-add-tcolorbox-newtcolorboxes
     '("note" "1" "[" "")
     '("Solution" "" "" ""))
